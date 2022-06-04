c8a83c8c3b117dab074be0a353861035671efffe
Shaurya Aher <shauryaaher1200@gmail.com>
2022-06-04 20:28:05 +0530

    Initial commit from Create Next App

---
+++ pages/time.tsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import Head from "next/head";
+
+type Timer = {
+  time: Object;
+};
+
+export default class Time extends Component<{}, Timer> {
+  interval: NodeJS.Timer;
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      time: new Date(),
+    };
+  }
+  setAnInterval() {
+    this.setState({
+      time: new Date(),
+    });
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => this.setAnInterval(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Current Time</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <h1>{this.state.time.toLocaleString()}</h1>
+      </>
+    );
+  }
+}
