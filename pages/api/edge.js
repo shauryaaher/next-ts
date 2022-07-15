ef1b89f5a08d5edf9f56d8a001f9f18272f66439
Shaurya Aher <shauryaaher1200@gmail.com>
2022-07-15 20:04:08 +0530

    Change types

--- pages/api/edge.js
+++ pages/api/edge.js
@@ -1,11 +1,11 @@
+import { NextResponse } from "next/server";
+
 export const config = {
   runtime: "experimental-edge",
 };
 
 export default async function toTheEdge(req, res) {
   if (req.method !== "GET") {
-    res.json({ hello: "world" });
-  } else {
-    res.status(200).json({ hello: "world" });
+    return NextResponse.json({ hello: "world" });
   }
 }
