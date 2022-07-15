316bf0df68ca4984402705ea50ccefd5839a3289
Shaurya Aher <shauryaaher1200@gmail.com>
2022-07-15 20:22:28 +0530

    Create an Edge Function

--- pages/api/edge.js
+++ pages/api/edge.js
@@ -1,11 +1,7 @@
-import { NextResponse } from "next/server";
-
 export const config = {
   runtime: "experimental-edge",
 };
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return NextResponse.json({ hello: "world" });
-  }
+  return new Response("Hello world!");
 }
