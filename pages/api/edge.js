a6e4544777272f9c7c1cda037f686e7ab7fde34d
Shaurya Aher <shauryaaher1200@gmail.com>
2022-07-16 14:40:58 +0530

    Edit the Edge function

--- pages/api/edge.js
+++ pages/api/edge.js
@@ -2,6 +2,6 @@ export const config = {
   runtime: "experimental-edge",
 };
 
-export default async function handler(req, res) {
-  return new Response({text: "Hello World!"});
+export default function toTheEdge(req, res) {
+  return new Response("Hello world!");
 }
